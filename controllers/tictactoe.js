1d551e4f0ec5047120c3963861411527efc0c66e
Amir Rajan <ar@amirrajan.net>
2014-02-01 09:54:13 -0600

    Rest APIs Getting Started (Part 3) [end]

--- controllers/tictactoe.js
+++ controllers/tictactoe.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var querystring = require('querystring');
 
 function toString(board) {
   var stringBoard = [];
@@ -10,6 +11,20 @@ function toString(board) {
   return stringBoard;
 }
 
+function turn(board) {
+  var x = 0;
+  var o = 0;
+
+  _.each(_.flatten(board), function(entry) {
+    if(entry == "x") x += 1;
+    if(entry == "o") o += 1;
+  });
+
+  if(x == o) return "x";
+
+  return 'o';
+}
+
 function init(app) {
   app.get('/tictactoe', function(req, res) {
     var board = [
@@ -18,15 +33,31 @@ function init(app) {
       [".", ".", "."],
     ];
 
-    if(req.params.topLeft) {
-      board[0][0] = "x";
+    var availableMoves = {
+      topleft: { row: 0, col: 0 },
+      topmiddle: { row: 0, col: 1 }
+    };
+
+    for(var key in availableMoves) {
+      var move = availableMoves[key];
+      if(req.params[key]) {
+        board[move.row][move.col] = req.params[key];
+      }
     }
+
+    var moves = { };
+
+    for(var key in availableMoves) {
+      var nextMove = JSON.parse(JSON.stringify(req.params));
+      nextMove[key] = turn(board);
+      if(!req.params[key]) {
+        moves[key] = "/tictactoe?" + querystring.stringify(nextMove);
+      }
+    };
 
     res.send({
       board: toString(board),
-      moves: {
-        topleft: "/tictactoe?topLeft=x"
-      }
+      moves: moves
     });
   });
 }
--- specification/tictactoe_spec.rb
+++ specification/tictactoe_spec.rb
@@ -1,14 +1,27 @@
 require './specification/spec_helper.rb'
 
 describe "tic toe" do
-  it "works", :focus => true do
+  it "moves that have been taken are no longer available", :focus => true do
     yaml = get "/tictactoe"
+
     expect(yaml["board"][0]).to eq("...")
     expect(yaml["board"][1]).to eq("...")
     expect(yaml["board"][2]).to eq("...")
 
     yaml = get yaml["moves"]["topleft"]
+    
     expect(yaml["board"][0]).to eq("x..")
+    expect(yaml["board"][1]).to eq("...")
+    expect(yaml["board"][2]).to eq("...")
+    expect(yaml["moves"]["topleft"]).to eq(nil)
+  end
+
+  it "works", :focus => true do
+    yaml = get "/tictactoe"
+    yaml = get yaml["moves"]["topleft"]
+    yaml = get yaml["moves"]["topmiddle"]
+
+    expect(yaml["board"][0]).to eq("xo.")
     expect(yaml["board"][1]).to eq("...")
     expect(yaml["board"][2]).to eq("...")
   end
